101109e058bb6ee13c3a0d026199fbdb42e937c2
HP <HP@HPLAPPY-RAMYA>
2024-12-04 13:35:30 +0530

    Add project files.

---
+++ WebApp1204/WeatherForecast.fs
@@ -0,0 +1,11 @@
+namespace WebApp1204
+
+open System
+
+type WeatherForecast =
+    { Date: DateTime
+      TemperatureC: int
+      Summary: string }
+
+    member this.TemperatureF =
+        32.0 + (float this.TemperatureC / 0.5556)
